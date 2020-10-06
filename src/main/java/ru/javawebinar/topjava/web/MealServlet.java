4466dbef9869110108658c39eca4df682e9152ae
Relb <relb@relb.su>
2020-10-06 08:04:54 +0300

    Add repository MealsData

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,21 +1,31 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.repository.MealsData;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<MealTo> listOfMealTo = MealsUtil.filteredByStreams(MealsData.getAll(), LocalTime.of(0,0), LocalTime.of(23,59,59), 2000);
+
+        req.setAttribute("listofmealto", listOfMealTo);
         log.debug("redirect to meals");
-        resp.sendRedirect("meals.jsp");
+        req.getRequestDispatcher("meals.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
 <html>
 <head>
     <title>Meals</title>
@@ -7,5 +10,15 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table class="meals-table">
+    <tr><th>Date</th><th>Description</th><th>Calories</th></tr>
+        <c:forEach items="${requestScope.listofmealto}" var="mealto">
+        <tr>
+            <td>${mealto.dateTime}</td>
+            <td>${mealto.description}</td>
+            <td>${mealto.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
